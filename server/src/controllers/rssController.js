2ec52cf71d38aee238410e6dd65ed65c4046cdab
binyominzeev <szvbinjomin@gmail.com>
2025-11-22 22:48:33 +0100

    rss2

--- server/src/controllers/rssController.js
+++ server/src/controllers/rssController.js
@@ -6,8 +6,8 @@ const { formatRSSFeed } = require('../utils/rssUtils');
 
 const router = express.Router();
 
-// Endpoint to serve user-specific RSS feeds
-router.get('/:username', async (req, res) => {
+// Controller function for user-specific RSS feed
+const getUserRssFeed = async (req, res) => {
   const { username } = req.params;
 
   try {
@@ -25,6 +25,6 @@ router.get('/:username', async (req, res) => {
     console.error('Error generating RSS feed:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
-});
+};
 
-module.exports = router;
+module.exports = { getUserRssFeed };
